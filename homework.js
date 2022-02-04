482686e3c743ded77d5f0d7b9d4818841bb979e0
SergelenE <97089494+SergelenE@users.noreply.github.com>
2022-02-04 15:04:35 +0800

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,51 @@
+/* Bodlogo 1 */
+// let myAlphabet = ["A", "B", "C", "D", "E", "F", "G"]
+// function myAlphabetLength(){
+//     console.log(myAlphabet.length);
+//     if(myAlphabet.length<4){
+//         console.log('Array urt 4-s baga baina')
+//     } else{
+//         console.log('Array urt 4-s ih baina')
+//     }
+// }
+// myAlphabetLength();
+
+/* Bodlogo 2 */
+
+// let planets = [prompt('Utga 1'),prompt('Utga 2'),prompt('Utga 3'),prompt('Utga 4'),prompt('Utga 5')];
+
+// let i=0;
+// while(i<5){
+//     console.log(planets[i] + ' index: ' + i);
+//     i++;
+// }
+
+/* Bodlogo 3 */
+
+// let wowDataTypes = ['Word213', 123, false, 'a', 1.24];
+
+// let type;
+// for(i=0; i<5; i++){
+//     type = typeof wowDataTypes[i];
+//     console.log(wowDataTypes[i], ' index:' + i + ' type: ' + type);
+// }
+
+
+/* Bodlogo 4 */
+// let student1Courses = ['Математик' , 'Англи хэл' , 'Програмчлал'];
+// let student2Courses = ['Газарзүй' , 'Испани хэл' , 'Програмчлал'];
+
+// let i = 0;
+// while(i<3){
+//     let j = 0;
+//     while(j<3){
+//         if(student1Courses [i] === student2Courses [j]){
+//             console.log(student1Courses [i]);
+//         }
+//         j++;
+//     }
+//     i++;
+// }
+
+
+
